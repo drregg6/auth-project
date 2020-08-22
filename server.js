cfd1b6b8835753288e36ff2390a71296208b6267
Dave Regg <dave.r.regg@gmail.com>
2020-08-22 16:11:05 -0400

    Setting up express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+// import DB and run
+
+const PORT = process.env.PORT || 3000;
+
+// get routes
+app.use(`/api/users`, require(`./routes/api/users.js`));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+})
