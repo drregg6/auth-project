a95f465fd781fc9af651a69d8dc360c4ffb895ca
Dave Regg <dave.r.regg@gmail.com>
2020-08-23 15:12:31 -0400

    Created User Schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,19 @@
-// create user schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('User', userSchema);
