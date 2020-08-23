dcf3fd7d3e082fc791e2d4d009a28d0946c930ab
Dave Regg <dave.r.regg@gmail.com>
2020-08-23 15:34:19 -0400

    Added express bodyparser to server and simple routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Hello world!');
+// Get all Users
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  console.log(users);
+  res.send('hello world');
 });
+
+
+
+// Get a User
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+});
+
+
+
+// Add user to database
+router.post('/', async (req, res) => {
+  // Get information from the user
+  console.log(req.body);
+  const { username, password } = req.body;
+  
+  // Create a new user to be saved into the database
+  const newUser = new User({
+    username,
+    password
+  });
+
+  try {
+    await newUser.save();
+    res.send('User created');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const connectDb = require('./config/db');
 connectDb();
 
+// Middleware and setup
 const PORT = process.env.PORT || 3000;
+app.use(express.json({ extended: false }))
 
 // get routes
 app.use(`/api/users`, require(`./routes/api/users.js`));
