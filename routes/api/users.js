dd2e58a49c6445f9159c0d90336a875eeb668b59
Dave Regg <dave.r.regg@gmail.com>
2020-08-23 15:47:14 -0400

    Sign in - check if user exists and hashed passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const router = express.Router();
 const User = require('../../models/User');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
 
 // Get all Users
 router.get('/', async (req, res) => {
@@ -22,14 +23,26 @@ router.get('/:id', (req, res) => {
 // Add user to database
 router.post('/', async (req, res) => {
   // Get information from the user
-  console.log(req.body);
   const { username, password } = req.body;
+
+  // Check if the user already exists
+  // findOne will return the user or null
+  const user = await User.findOne({ username });
+  console.log(user);
+  if (user) {
+    return res.send('User already exists. Please sign in.');
+  }
   
   // Create a new user to be saved into the database
   const newUser = new User({
     username,
     password
   });
+
+  // Crypt the password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  newUser.password = hash;
 
   try {
     await newUser.save();
