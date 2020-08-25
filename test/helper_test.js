f207b134d82af04765277dc1a963d6c94d6121d3
Dave Regg <dave.r.regg@gmail.com>
2020-08-25 19:21:57 -0400

    mongoose helper test file written

---
+++ test/helper_test.js
@@ -0,0 +1,19 @@
+// helper functions for tests
+const connectDb = require('../config/db');
+const mongoose = require('mongoose');
+// Needed when using ES6 Promises
+mongoose.Promise = global.Promise;
+
+
+before((done) => {
+  // Setup connection to the database
+  // connect to db
+  connectDb();
+  // .once and .on are Promises used for databases
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connected');
+      done();
+    })
+    .on('error', (error) => console.log(error))
+});
