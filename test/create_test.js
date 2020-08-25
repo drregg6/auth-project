39954ba46a88ee4345bce8820b76221bd4c2c746
Dave Regg <dave.r.regg@gmail.com>
2020-08-25 19:37:49 -0400

    Sample test

---
+++ test/create_test.js
@@ -0,0 +1,8 @@
+const User = require('../models/User');
+const assert = require('assert'); // provided by mocha
+
+describe('User', () => {
+  it('should create a user in the db', () => {
+    assert(true);
+  });
+});
