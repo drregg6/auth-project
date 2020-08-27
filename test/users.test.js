a6dfcf4a6dd07f52a9bd771edfcfab13b713a645
Dave Regg <dave.r.regg@gmail.com>
2020-08-27 15:54:49 -0400

    Added two more tests in the User Update block

--- test/users.test.js
+++ test/users.test.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../config/app');
 
-
+// Populate database with users
 const user1 = new User({ username: 'user1', password: 'password' });
 const user2 = new User({ username: 'user2', password: 'password' });
 
-// initialize database
+// Initialize database
 beforeEach(async () => {
   await User(user1).save();
   await User(user2).save();
 });
 
-// drop database after each test
+// Drop database after each test
 afterEach(async () => {
   await mongoose.connection.collections.users.drop();
 });
@@ -50,29 +50,51 @@ describe('READ USER', () => {
 
 // Update User
 describe('UPDATE USER', () => {
+  console.log(user1.password);
+
   it('should fail if passwords do not match', async () => {
+    let testUser = {
+      username: user1.username,
+      password: user1.password,
+      newPassword: 'newPassword',
+      repeatPassword: 'pasword'
+    };
     const response = await request(app)
       .put('/api/users/update')
-      .send({
-        username: user1.username,
-        password: user1.password,
-        newPassword: 'newPassword',
-        repeatPassword: 'pasword'
-      });
+      .send(testUser);
     let { body, status } = response;
     expect(status).toBe(401);
     expect(body.msg).toBe('Passwords do not match.');
   });
-  it('should update a user', async () => {
-    const response = await request(app)
+
+  it('should contain the user being updated', async () => {
+    let testUser = {
+      username: 'newUser',
+      password: 'password',
+      newPassword: 'newPassword',
+      repeatPassword: 'newPassword'
+    }
+    const res = await request(app)
       .put('/api/users/update')
-      .send({
-        username: user1.username,
-        password: user1.password,
-        newPassword: 'password1',
-        repeatPassword: 'password1'
-      });
-    console.log(response);
+      .send(testUser);
+    let { body, status } = res;
+    expect(status).toBe(401);
+    expect(body.msg).toBe('User not found.');
+  });
+
+  it('should update a user', async () => {
+    let testUser = {
+      username: 'user1',
+      password: 'password',
+      newPassword: 'password1',
+      repeatPassword: 'password1'
+    };
+    const res = await request(app)
+      .put('/api/users/update')
+      .send(testUser);
+    const { status, body } = res;
+    console.log(`user1's password is: ${user1.password}`);
+    console.log(status);
   });
 });
 
