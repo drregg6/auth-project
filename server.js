77c173ba510a0f464f716cbbbca27ec0208cd817
Dave Regg <dave.r.regg@gmail.com>
2020-10-13 18:56:29 -0400

    Prepping for deployment

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ TODOs
 = Go over logging in and auth routes
 
 */
-
+const path = require('path');
 const app = require('./config/app');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
