8a3155774c1f9bab133f665e95e7b3315f973ddf
Dave Regg <dave.r.regg@gmail.com>
2020-10-13 12:06:07 -0400

    Added some goofy images

--- client/src/components/private/Private.js
+++ client/src/components/private/Private.js
@@ -1,18 +1,27 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import Image1 from '../../images/private-bg/dan-dennis-giraffe.jpg';
+import Image2 from '../../images/private-bg/diane-alkier-banana.jpg';
+import Image3 from '../../images/private-bg/ryan-franco-child.jpg';
+import Image4 from '../../images/private-bg/ryan-grewell-bear.jpg';
 
 import Main from '../layout/Main';
 
 const Private = props => {
+  const backgrounds = [ Image1, Image2, Image3, Image4 ];
+  const backgroundsLength = backgrounds.length;
+  let randNum = Math.floor(Math.random() * backgroundsLength);
+  let background = backgrounds[randNum];
   return (
     <Main mainNoPadding>
-      <div className="private" style={{ padding: 0 }}></div>
+      <div
+        className="private"
+        style={{
+          padding: 0,
+          backgroundImage: `url(${background})`
+        }}
+      ></div>
     </Main>
   )
 }
-
-// Private.propTypes = {
-
-// }
 
 export default Private;
