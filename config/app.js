14d24f873e7cb6cefeb0d8150a427beb222855a5
Dave Regg <dave.r.regg@gmail.com>
2020-10-14 16:42:43 -0400

    Moved code from db --> app

--- config/app.js
+++ config/app.js
@@ -13,4 +13,12 @@ app.use(express.json({ extended: false }))
 app.use(`/api/users`, require(`../routes/api/users`));
 app.use(`/api/auth`, require('../routes/api/auth'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 module.exports = app;
